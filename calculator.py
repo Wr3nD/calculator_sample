3bcd0037a0f5af919f880e5d68205e6a8f88369e
Wr3nD <radostavit10@gmail.com>
2024-10-05 10:47:17 +0200

    add of calculator function

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def ask_user():
+    print('original number:')
+    number = int(input())
+    print('divisor')
+    is_correct = False
+    while not is_correct:
+        try:
+            divisor = int(input())
+            is_correct = True
+        except ValueError:
+            print('zadaj korektne cislo') 
+    print(get_count(number,divisor))
